6194c3e4045ba978a542d78aba2f68fa29792fef
Fatebahadur Nandwanshi <122743534+KingFate111@users.noreply.github.com>
2026-02-25 07:38:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main args) {
         System.out.println("Hello, World! Main world"); // Prints the message to the console
     }
 }
+
 
 
