620afcf5a1d3948a37442355dcb7ce4d111dcb87
Fatebahadur Nandwanshi <122743534+KingFate111@users.noreply.github.com>
2026-02-25 07:40:26 +0530

    Fix main method syntax in HelloWorld class

--- hello.java
+++ hello.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
-    public static void main args) {
+    public static void main (String[]args) {
         System.out.println("Hello, World! Main world"); // Prints the message to the console
     }
 }
+
 
 
 
